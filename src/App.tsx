8f55019f5c050590c55dc7a1aee54492bf64f3a0
snowkwon420 <snowkwon420@gmail.com>
2024-09-04 15:51:36 +0900

    test: TypeScript migration test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { RecoilRoot } from 'recoil';
-import Routing from './routes/Routing';
-import GlobalStyle from './styles/globalstyle';
+import Routing from './routes/Routing.jsx';
+import GlobalStyle from './styles/globalstyle.js';
 import styled from 'styled-components';
 
-function App() {
+const App: React.FC = () => {
   return (
     <Wrap>
-    <RecoilRoot>
-      <GlobalStyle />
-      <Routing />
-    </RecoilRoot>
+      <RecoilRoot>
+        <GlobalStyle />
+        <Routing />
+      </RecoilRoot>
     </Wrap>
   );
-}
+};
 
 const Wrap = styled.div`
   position: relative;
@@ -21,5 +21,5 @@ const Wrap = styled.div`
   margin: 0 auto;
   box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
   overflow: hidden;
-`
+`;
 export default App;
